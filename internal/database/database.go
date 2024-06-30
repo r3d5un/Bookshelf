d2f6e9e22e14641adece4c7dd9d2b06487f37a56
r3d5un <r3d5un@pm.me>
2024-07-01 00:53:01 +0200

    Create a database package

---
+++ internal/database/database.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"time"
+)
+
+func OpenPool(
+	connString string,
+	maxOpenConns int,
+	maxIdleConns int,
+	maxIdleTime string,
+	timeout time.Duration,
+) (*sql.DB, error) {
+	db, err := sql.Open("pgx", connString)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetMaxIdleConns(maxIdleConns)
+
+	duration, err := time.ParseDuration(maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
