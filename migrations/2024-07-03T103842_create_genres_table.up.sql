3da00eba3a4df89caae50e126f7dfea963c1b744
r3d5un <r3d5un@pm.me>
2024-07-04 22:47:36 +0200

    Create migration for genres table

---
+++ migrations/2024-07-03T103842_create_genres_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS books.genres
+(
+    id           UUID                  DEFAULT gen_random_uuid() PRIMARY KEY,
+    name         VARCHAR(256) NOT NULL UNIQUE,
+    description  TEXT         NULL,
+    created_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_updated TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
