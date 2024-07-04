18084d277200a8bdedec73b2b13fd81e902b9fa7
r3d5un <r3d5un@pm.me>
2024-07-04 22:46:57 +0200

    Add timestamp default value

--- migrations/2024-07-03T103817_create_authors_table.up.sql
+++ migrations/2024-07-03T103817_create_authors_table.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS books.authors
 (
-    id          UUID                  DEFAULT gen_random_uuid() PRIMARY KEY,
-    name        VARCHAR(512) NOT NULL,
-    description TEXT         NULL,
-    website     varchar(256) NULL,
-    created_at  TIMESTAMP    NOT NULL DEFAULT NOW(),
-    updated_at  TIMESTAMP    NOT NULL DEFAULT NOW()
+    id           UUID                  DEFAULT gen_random_uuid() PRIMARY KEY,
+    name         VARCHAR(512) NOT NULL,
+    description  TEXT         NULL,
+    website      varchar(256) NULL,
+    created_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_updated TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
