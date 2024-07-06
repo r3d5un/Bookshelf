da77a9abc0b71719f6b22bb83ad535e593ff3a1d
r3d5un <r3d5un@pm.me>
2024-07-07 00:10:18 +0200

    Create Update method for the BookModel

--- internal/books/data/books.go
+++ internal/books/data/books.go
@@ -228,3 +228,72 @@ RETURNING
 	logger.Info("returning inserted book", "insertedBook", b)
 	return b, nil
 }
+
+func (m *BookModel) Update(ctx context.Context, newBook Book) (b *Book, err error) {
+	logger := logging.LoggerFromContext(ctx)
+
+	query := `
+UPDATE books.books
+SET id          = COALESCE($1, id),
+    title       = COALESCE($2, title),
+    author_id   = COALESCE($3, author_id),
+    description = COALESCE($4, description),
+    published   = COALESCE($5, published),
+    created_at  = COALESCE($6, created_at),
+    updated_at  = COALESCE($7, updated_at)
+WHERE id = $1
+RETURNING
+    id,
+    title,
+    author_id,
+    description,
+    published,
+    created_at,
+    updated_at;
+`
+
+	qCtx, cancel := context.WithTimeout(ctx, *m.Timeout)
+	defer cancel()
+
+	logger = logger.With(
+		slog.Group(
+			"query",
+			slog.String("statement", database.MinifySQL(query)),
+			"newBook", newBook,
+		),
+	)
+
+	logger.Info("performing query")
+	err = m.DB.QueryRowContext(
+		qCtx,
+		query,
+		newBook.ID,
+		newBook.Title,
+		newBook.AuthorID,
+		newBook.Description,
+		newBook.Published,
+		newBook.CreatedAt,
+		newBook.UpdatedAt,
+	).Scan(
+		&b.ID,
+		&b.Title,
+		&b.AuthorID,
+		&b.Description,
+		&b.Published,
+		&b.CreatedAt,
+		&b.UpdatedAt,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			logger.Info("no record found", "error", err)
+			return nil, ErrRecordNotFound
+		default:
+			logger.Error("unable to perform query", "error", err)
+			return nil, err
+		}
+	}
+
+	logger.Info("returning updated book", "updatedBook", b)
+	return b, nil
+}
