1edd691f53efa2ec9352ea4b50a75dcad6320df0
r3d5un <r3d5un@pm.me>
2024-07-07 23:40:42 +0200

    Add timestamp columns to series table

--- migrations/2024-07-04T211032_create_series_table.up.sql
+++ migrations/2024-07-04T211032_create_series_table.up.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS books.series (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
-    description TEXT NULL
+    description TEXT NULL,
+    created_at TIMESTAMP NULL,
+    updated_at TIMESTAMP NULL
 );
