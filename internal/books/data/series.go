8e9b94d665f6ffeafc78b6213b2f95b3674fdc4a
r3d5un <r3d5un@pm.me>
2024-07-07 23:49:56 +0200

    Create method GetAll for the SeriesModel

--- internal/books/data/series.go
+++ internal/books/data/series.go
@@ -69,3 +69,80 @@ WHERE id = $1;
 	logger.Info("returning series")
 	return series, nil
 }
+
+func (m *SeriesModel) GetAll(
+	ctx context.Context,
+	filters Filters,
+) (series []*Series, totalResults *int, err error) {
+	logger := logging.LoggerFromContext(ctx)
+
+	query := `
+SELECT id,
+       name,
+       description,
+       created_at,
+       updated_at
+FROM books.series
+WHERE ($1 IS NULL OR id = $1)
+  AND ($2 IS NULL OR name LIKE '%' || $2 || '%')
+  AND ($3 IS NULL OR description LIKE '%' || $3 || '%')
+  AND ($4 IS NULL OR created_at >= $4)
+  AND ($5 IS NULL OR created_at < $5)
+  AND ($6 IS NULL OR updated_at >= $6)
+  AND ($7 IS NULL OR updated_at < $7)
+` + database.CreateOrderByClause(filters.OrderBy) + `
+OFFSET $9 FETCH NEXT $10 ROWS ONLY;
+`
+
+	qCtx, cancel := context.WithTimeout(ctx, *m.Timeout)
+	defer cancel()
+
+	logger = logger.With(
+		"query",
+		slog.String("statement", database.MinifySQL(query)),
+		"filters", filters,
+	)
+
+	logger.Info("performing query")
+	rows, err := m.DB.QueryContext(
+		qCtx,
+		query,
+		filters.ID,
+		filters.Name,
+		filters.Description,
+		filters.Website,
+		filters.CreatedAtFrom,
+		filters.CreatedAtTo,
+		filters.UpdatedAtFrom,
+		filters.UpdatedAtTo,
+	)
+	if err != nil {
+		logger.Error("error performing query", "error", err)
+		return nil, nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var s Series
+
+		err := rows.Scan(
+			&s.ID,
+			&s.Name,
+			&s.Description,
+			&s.CreatedAt,
+			&s.UpdatedAt,
+		)
+		if err != nil {
+			return nil, nil, err
+		}
+		series = append(series, &s)
+	}
+	if err = rows.Err(); err != nil {
+		logger.Error("an error occurred while parsing query results", "error", err)
+		return nil, nil, err
+	}
+	numberOfRecords := len(series)
+
+	logger.Info("returning records", slog.Int("records", numberOfRecords))
+	return series, &numberOfRecords, nil
+}
