199746265b906fcdc458ad07caa6f83785bf9789
r3d5un <r3d5un@pm.me>
2024-07-07 20:49:55 +0200

    Create Get method for GenreModel

---
+++ internal/books/data/genres.go
@@ -0,0 +1,71 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"log/slog"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/r3d5un/Bookshelf/internal/database"
+	"github.com/r3d5un/Bookshelf/internal/logging"
+)
+
+type Genre struct {
+	ID          uuid.UUID  `json:"id"`
+	Name        string     `json:"name"`
+	Description *string    `json:"description"`
+	CreatedAt   *time.Time `json:"created_at"`
+	UpdatedAt   *time.Time `json:"updated_at"`
+}
+
+type GenreModel struct {
+	DB      *sql.DB
+	Timeout *time.Duration
+}
+
+func (m *GenreModel) Get(ctx context.Context, id uuid.UUID) (genre *Genre, err error) {
+	logger := logging.LoggerFromContext(ctx)
+
+	query := `
+SELECT id,
+       name,
+       description,
+       created_at,
+       updated_at
+FROM books.genres
+WHERE id = $1;
+`
+
+	qCtx, cancel := context.WithTimeout(ctx, *m.Timeout)
+	defer cancel()
+
+	logger = logger.With(
+		"query",
+		slog.String("statement", database.MinifySQL(query)),
+		slog.String("id", id.String()),
+	)
+
+	logger.Info("performing query")
+	err = m.DB.QueryRowContext(qCtx, query, id.String()).Scan(
+		&genre.ID,
+		&genre.Name,
+		&genre.Description,
+		&genre.CreatedAt,
+		&genre.UpdatedAt,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			logger.Info("no rows found", "group_id", id.String())
+			return nil, ErrRecordNotFound
+		default:
+			logger.Info("an error occurred while performing query", "error", err)
+			return nil, err
+		}
+	}
+
+	logger.Info("returning author")
+	return genre, nil
+}
