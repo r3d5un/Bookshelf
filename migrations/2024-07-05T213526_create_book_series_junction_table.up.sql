00fdbfcb13c56009a2aacf16a4b94a04a6472f4e
r3d5un <r3d5un@pm.me>
2024-07-10 18:56:01 +0200

    Use the correct target table for the series foregin key

--- migrations/2024-07-05T213526_create_book_series_junction_table.up.sql
+++ migrations/2024-07-05T213526_create_book_series_junction_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS books.book_series
 (
     book_id      UUID  NOT NULL REFERENCES books.books (id) ON DELETE CASCADE,
-    series_id    UUID  NOT NULL REFERENCES books.authors (id) ON DELETE CASCADE,
+    series_id    UUID  NOT NULL REFERENCES books.series (id) ON DELETE CASCADE,
     series_order FLOAT NULL
 );
