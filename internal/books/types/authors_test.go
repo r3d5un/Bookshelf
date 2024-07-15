a298730236284d04de40ad10405b2f0afeb8e682
r3d5un <r3d5un@pm.me>
2024-07-15 04:18:41 +0200

    Create test for the ReadAuthor function

--- internal/books/types/authors_test.go
+++ internal/books/types/authors_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	// "github.com/google/uuid"
+	"github.com/google/uuid"
+	"github.com/r3d5un/Bookshelf/internal/books/data"
 	"github.com/r3d5un/Bookshelf/internal/books/types"
 )
 
@@ -19,6 +21,8 @@ func TestComplexAuthorTypes(t *testing.T) {
 	// 	Website:     &website,
 	// }
 
+	var id *uuid.UUID
+
 	t.Run("TestCreateAuthor", func(t *testing.T) {
 		newAuthor := types.NewAuthorData{
 			Name:        authorName,
@@ -26,14 +30,20 @@ func TestComplexAuthorTypes(t *testing.T) {
 			Website:     &website,
 		}
 
-		_, err := types.CreateAuthor(context.Background(), models, newAuthor)
+		newAuthorID, err := types.CreateAuthor(context.Background(), models, newAuthor)
 		if err != nil {
 			t.Errorf("error occurred while registering a new author: %s\n", err)
 			return
 		}
+
+		id = newAuthorID
 	})
 
 	t.Run("TestReadAuthor", func(t *testing.T) {
+		if _, err := types.ReadAuthor(context.Background(), models, *id); err != nil {
+			t.Errorf("error occurred while retrieving author: %s\n", err)
+			return
+		}
 	})
 
 	t.Run("TestReadAllAuthor", func(t *testing.T) {
