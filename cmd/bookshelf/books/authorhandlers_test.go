8d0c472cb29a8d7099f36d8dc4df89dcbbb00646
r3d5un <r3d5un@pm.me>
2024-07-15 05:25:15 +0200

    Create test for the PostAuthorHandler

---
+++ cmd/bookshelf/books/authorhandlers_test.go
@@ -0,0 +1,64 @@
+package books_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/r3d5un/Bookshelf/internal/books/types"
+)
+
+func TestAuthorHandlers(t *testing.T) {
+	authorName := "Brandon Sanderson"
+	description := "such books. many words. wow."
+	website := "www.brandonsanderson.com"
+
+	t.Run("TestPostAuthorHandler", func(t *testing.T) {
+		newAuthor := types.NewAuthorData{
+			Name:        authorName,
+			Description: &description,
+			Website:     &website,
+		}
+
+		body, err := json.Marshal(newAuthor)
+		if err != nil {
+			t.Errorf("unable to marhsal author data: %s\n", err)
+			return
+		}
+
+		postReq := httptest.NewRequest(
+			http.MethodPost,
+			"/api/v1/bookshelf/authors",
+			strings.NewReader(string(body)),
+		)
+		postReq.Header.Set("Content-Type", "application/json")
+
+		rr := httptest.NewRecorder()
+
+		handler := http.HandlerFunc(mod.PostAuthorHandler)
+		handler.ServeHTTP(rr, postReq)
+
+		if status := rr.Code; status != http.StatusCreated {
+			t.Errorf(
+				"handler returned wrong error code: got %d, expected %d",
+				status,
+				http.StatusCreated,
+			)
+			return
+		}
+	})
+
+	t.Run("TestGetAuthorHandler", func(t *testing.T) {
+	})
+
+	t.Run("TestListAuthorHandler", func(t *testing.T) {
+	})
+
+	t.Run("TestPatchAuthorHandler", func(t *testing.T) {
+	})
+
+	t.Run("TestDeleteAuthorHandler", func(t *testing.T) {
+	})
+}
