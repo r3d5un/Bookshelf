cb82fdaa737a3d390fca5b36842d7c7c07549199
r3d5un <r3d5un@pm.me>
2024-07-15 02:21:12 +0200

    Create GetAuthorHandler

--- cmd/bookshelf/books/authorhandlers.go
+++ cmd/bookshelf/books/authorhandlers.go
@@ -1,9 +1,12 @@
 package books
 
 import (
+	"errors"
 	"fmt"
+	"log/slog"
 	"net/http"
 
+	"github.com/r3d5un/Bookshelf/internal/books/data"
 	"github.com/r3d5un/Bookshelf/internal/books/types"
 	"github.com/r3d5un/Bookshelf/internal/logging"
 	"github.com/r3d5un/Bookshelf/internal/rest"
@@ -33,4 +36,35 @@ func (m *Module) PostAuthorHandler(w http.ResponseWriter, r *http.Request) {
 
 	logger.Info("writing response")
 	rest.Respond(w, r, http.StatusCreated, nil, nil)
+}
+
+func (m *Module) GetAuthorHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	logger := logging.LoggerFromContext(ctx)
+
+	logger.Info("parsing ID")
+	id, err := rest.ReadUUIDParam("id", r)
+	if err != nil {
+		logger.Info("unable to read id", "id", id, "error", err)
+		rest.NotFoundResponse(w, r)
+		return
+	}
+	logger.Info("ID parsed", slog.String("id", id.String()))
+
+	logger.Info("querying database for obtain author by id")
+	author, err := types.ReadAuthor(ctx, &m.models, *id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			logger.Info("author not found", "id", id)
+			rest.NotFoundResponse(w, r)
+		default:
+			logger.Error("unable to get author", "id", id, "error", err)
+			rest.ServerErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	logger.Info("writing response")
+	rest.Respond(w, r, http.StatusOK, author, nil)
 }
