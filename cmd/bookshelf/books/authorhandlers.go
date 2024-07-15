2f2bfbd74d48726defb90445661acbec675d0568
r3d5un <r3d5un@pm.me>
2024-07-15 02:29:00 +0200

    Create DeleteAuthorHandler

--- cmd/bookshelf/books/authorhandlers.go
+++ cmd/bookshelf/books/authorhandlers.go
@@ -164,3 +164,34 @@ func (m *Module) PatchAuthorHandler(w http.ResponseWriter, r *http.Request) {
 	logger.Info("writing response")
 	rest.Respond(w, r, http.StatusOK, updatedAuthor, nil)
 }
+
+func (m *Module) DeleteAuthorHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	logger := logging.LoggerFromContext(ctx)
+
+	logger.Info("parsing ID")
+	id, err := rest.ReadUUIDParam("id", r)
+	if err != nil {
+		logger.Info("unable to read id", "id", id, "error", err)
+		rest.NotFoundResponse(w, r)
+		return
+	}
+	logger.Info("ID parsed", slog.String("id", id.String()))
+
+	logger.Info("deleting author", "id", id)
+	if err := types.DeleteAuthor(ctx, &m.models, *id); err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			logger.Info("author not found", "id", id)
+			rest.NotFoundResponse(w, r)
+		default:
+			logger.Error("unable to delete author", "id", id, "error", err)
+			rest.ServerErrorResponse(w, r, err)
+		}
+		return
+	}
+	logger.Info("author deleted")
+
+	logger.Info("writing response")
+	rest.Respond(w, r, http.StatusNoContent, nil, nil)
+}
