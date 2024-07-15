362a9def70bc7de389ca5f8ce57af18fb02e288b
r3d5un <r3d5un@pm.me>
2024-07-15 05:33:13 +0200

    Create test for the GetAuthorHandler

--- cmd/bookshelf/books/authorhandlers_test.go
+++ cmd/bookshelf/books/authorhandlers_test.go
@@ -1,6 +1,7 @@
 package books_test
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -14,6 +15,14 @@ func TestAuthorHandlers(t *testing.T) {
 	authorName := "Brandon Sanderson"
 	description := "such books. many words. wow."
 	website := "www.brandonsanderson.com"
+
+	newAuthor := types.NewAuthorData{
+		Name: "Steven Erikson",
+	}
+	id, err := types.CreateAuthor(context.Background(), models, newAuthor)
+	if err != nil {
+		t.Errorf("uanble to insert test data: %s\n", err)
+	}
 
 	t.Run("TestPostAuthorHandler", func(t *testing.T) {
 		newAuthor := types.NewAuthorData{
@@ -51,6 +60,27 @@ func TestAuthorHandlers(t *testing.T) {
 	})
 
 	t.Run("TestGetAuthorHandler", func(t *testing.T) {
+		getReq := httptest.NewRequest(
+			http.MethodGet,
+			"/api/v1/bookshelf/books/authors",
+			nil,
+		)
+		getReq.Header.Set("Content-Type", "application/json")
+		getReq.SetPathValue("id", id.String())
+
+		rr := httptest.NewRecorder()
+
+		handler := http.HandlerFunc(mod.GetAuthorHandler)
+		handler.ServeHTTP(rr, getReq)
+
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf(
+				"handler returned the wrong error code: got %d, expected %d\n",
+				status,
+				http.StatusOK,
+			)
+			return
+		}
 	})
 
 	t.Run("TestListAuthorHandler", func(t *testing.T) {
