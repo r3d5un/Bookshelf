2236fcafc75153a3861ed0b612d97b9aae5ac9ad
r3d5un <r3d5un@pm.me>
2024-07-15 13:07:07 +0200

    Add function ReadSeries

--- internal/books/types/series.go
+++ internal/books/types/series.go
@@ -2,6 +2,8 @@ package types
 
 import (
 	"context"
+	"sync"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/r3d5un/Bookshelf/internal/books/data"
@@ -10,6 +12,15 @@ import (
 type NewSeriesData struct {
 	Name        string  `json:"name"`
 	Description *string `json:"description,omitempty"`
+}
+
+type Series struct {
+	ID          uuid.UUID  `json:"id,omitempty"`
+	Name        *string    `json:"name,omitempty"`
+	Description *string    `json:"description,omitempty"`
+	CreatedAt   *time.Time `json:"created_at,omitempty"`
+	UpdatedAt   *time.Time `json:"updated_at,omitempty"`
+	Books       []*Book    `json:"books,omitempty"`
 }
 
 func CreateSeries(
@@ -29,4 +40,59 @@ func CreateSeries(
 	}
 
 	return &insertedSeries.ID, nil
+}
+
+func ReadSeries(ctx context.Context, models *data.Models, seriesID uuid.UUID) (*Series, error) {
+	seriesRecord, err := models.Series.Get(ctx, seriesID)
+	if err != nil {
+		return nil, err
+	}
+
+	seriesData := Series{
+		ID:          seriesRecord.ID,
+		Name:        &seriesRecord.Name,
+		Description: seriesRecord.Description,
+		CreatedAt:   seriesRecord.CreatedAt,
+		UpdatedAt:   seriesRecord.UpdatedAt,
+	}
+
+	bookRecords, totalBookRecords, err := models.Books.GetBySeriesID(ctx, seriesID)
+	if err != nil {
+		return nil, err
+	}
+	if *totalBookRecords < 1 {
+		return &seriesData, nil
+	}
+
+	var wg sync.WaitGroup
+	var authorDataMu sync.Mutex
+
+	errorChan := make(chan error, *totalBookRecords)
+
+	for _, bookRecord := range bookRecords {
+		wg.Add(1)
+		go func(ctx context.Context, models *data.Models, id uuid.UUID) {
+			defer wg.Done()
+
+			bookData, err := ReadBook(ctx, models, id)
+			if err != nil {
+				errorChan <- err
+			}
+
+			authorDataMu.Lock()
+			seriesData.Books = append(seriesData.Books, bookData)
+			authorDataMu.Unlock()
+		}(ctx, models, bookRecord.ID)
+	}
+
+	wg.Wait()
+	close(errorChan)
+
+	for err := range errorChan {
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &seriesData, nil
 }
--- internal/books/types/series_test.go
+++ internal/books/types/series_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/r3d5un/Bookshelf/internal/books/types"
 )
 
@@ -11,19 +12,27 @@ func TestComplexSeriesTypes(t *testing.T) {
 	seriesName := "Mistborn"
 	description := "Some series description"
 
+	var id *uuid.UUID
+
 	t.Run("TestCreateSeries", func(t *testing.T) {
 		newSeries := types.NewSeriesData{
 			Name:        seriesName,
 			Description: &description,
 		}
 
-		_, err := types.CreateSeries(context.Background(), models, newSeries)
+		newSeriesID, err := types.CreateSeries(context.Background(), models, newSeries)
 		if err != nil {
 			t.Errorf("error occurred while registering a new author: %s\n", err)
 			return
 		}
+
+		id = newSeriesID
 	})
 
 	t.Run("TestReadSeries", func(t *testing.T) {
+		if _, err := types.ReadSeries(context.Background(), models, *id); err != nil {
+			t.Errorf("error occurred while retrieving series: %s\n", err)
+			return
+		}
 	})
 }
