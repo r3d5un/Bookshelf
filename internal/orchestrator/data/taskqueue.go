2b5386b056c60f5fce3bc5f91ff568c425122840
r3d5un <r3d5un@pm.me>
2024-08-22 22:10:28 +0200

    Create Listen method for the TaskQueueModel

--- internal/orchestrator/data/taskqueue.go
+++ internal/orchestrator/data/taskqueue.go
@@ -2,10 +2,14 @@ package data
 
 import (
 	"context"
+	"log/slog"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/r3d5un/Bookshelf/internal/database"
+	"github.com/r3d5un/Bookshelf/internal/logging"
 )
 
 const (
@@ -28,4 +32,65 @@ type TaskQueueModel struct {
 	Timeout *time.Duration
 	Pool    *pgxpool.Pool
 }
+
+func (m *TaskQueueModel) Listen(
+	ctx context.Context,
+	notificationCh chan<- pgconn.Notification,
+	done <-chan bool,
+) {
+	logger := logging.LoggerFromContext(ctx)
+
+	query := `
+LISTEN task_queue_notification;
+`
+
+	logger = logger.With(
+		slog.Group(
+			"query",
+			slog.String("statement", database.MinifySQL(query)),
+		),
+	)
+
+	for {
+		conn, err := m.Pool.Acquire(ctx)
+		if err != nil {
+			logger.Error("unable to acquire connection", "error", err)
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		logger.Info("listening for task notifications")
+		_, err = conn.Exec(context.Background(), query)
+		if err != nil {
+			logger.Error("unable to listen", "error", err)
+			conn.Release()
+			continue
+		}
+
+		for {
+			select {
+			case <-done:
+				logger.Info("received done signal, stopping listener.")
+				return
+			default:
+				if notification, err := conn.Conn().WaitForNotification(ctx); err != nil {
+					logger.Info("unable to receive notification", "error", err)
+					break
+				} else {
+					select {
+					case notificationCh <- *notification:
+						logger.Info("notification sent to channel")
+					case <-done:
+						logger.Info("received done signal, stopping listener.")
+						return
+					default:
+						logger.Error("channel full, notification not sent")
+						time.Sleep(5 * time.Second)
+						continue
+					}
+				}
+			}
+		}
+
+	}
 }
