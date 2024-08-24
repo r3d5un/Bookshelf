c6df49c28fe15e7317e4234b2d9ecc32dd8666cd
r3d5un <r3d5un@pm.me>
2024-08-25 00:53:49 +0200

    Create test for the ReadAllTasks function

--- internal/orchestrator/types/tasks_test.go
+++ internal/orchestrator/types/tasks_test.go
@@ -37,4 +37,24 @@ func TestTaskTypes(t *testing.T) {
 			t.Errorf("error occurred while reading task: %s\n", err)
 		}
 	})
+
+	t.Run("ReadAllTasks", func(t *testing.T) {
+		filters := data.Filters{
+			Page:     1,
+			PageSize: 100,
+			OrderBy:  []string{"id"},
+		}
+		tc, err := types.ReadAllTasks(context.Background(), models, filters)
+		if err != nil {
+			t.Errorf("error occurred while reading tasks: %s\n", err)
+		}
+		if len(tc.Data) < 1 {
+			t.Errorf("no tasks returned")
+			return
+		}
+		if tc.CurrentPage != filters.Page {
+			t.Errorf("expected page %d in metadata, got %d", filters.Page, tc.CurrentPage)
+			return
+		}
+	})
 }
