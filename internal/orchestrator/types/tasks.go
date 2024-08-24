c3ac8ff95ce167963d0bb9f481ea4b268b837b6f
r3d5un <r3d5un@pm.me>
2024-08-24 22:47:54 +0200

    Create function for reading all tasks

--- internal/orchestrator/types/tasks.go
+++ internal/orchestrator/types/tasks.go
@@ -17,6 +17,16 @@ type Task struct {
 	RunAt     *time.Time `json:"runAt"`
 }
 
+type TaskCollection struct {
+	CurrentPage  int     `json:"current_page,omitempty"`
+	PageSize     int     `json:"page_size,omitempty"`
+	FirstPage    int     `json:"first_page,omitempty"`
+	LastPage     int     `json:"last_page,omitempty"`
+	TotalRecords int     `json:"total_records,omitempty"`
+	OrderBy      string  `json:"order_by,omitempty"`
+	Data         []*Task `json:"data"`
+}
+
 func ReadTask(ctx context.Context, models *data.Models, taskID uuid.UUID) (*Task, error) {
 	tq, err := models.TaskQueues.Get(ctx, taskID)
 	if err != nil {
@@ -33,4 +43,40 @@ func ReadTask(ctx context.Context, models *data.Models, taskID uuid.UUID) (*Task
 	}
 
 	return &task, nil
+}
+
+func ReadAllTasks(
+	ctx context.Context,
+	models *data.Models,
+	filters data.Filters,
+) (tc *TaskCollection, err error) {
+	tq, metadata, err := models.TaskQueues.GetAll(ctx, filters)
+	if err != nil {
+		return nil, err
+	}
+
+	var tasks []*Task
+	for _, t := range tq {
+		task := Task{
+			ID:        t.ID,
+			Queue:     t.Queue,
+			State:     t.State,
+			CreatedAt: t.CreatedAt,
+			UpdatedAt: t.UpdatedAt,
+			RunAt:     t.RunAt,
+		}
+
+		tasks = append(tasks, &task)
+	}
+
+	tc = &TaskCollection{
+		CurrentPage:  metadata.CurrentPage,
+		PageSize:     metadata.PageSize,
+		FirstPage:    metadata.FirstPage,
+		LastPage:     metadata.LastPage,
+		TotalRecords: metadata.TotalRecords,
+		Data:         tasks,
+	}
+
+	return tc, nil
 }
