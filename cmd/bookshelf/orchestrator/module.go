5943a38c2f25b3e70a726401e6c32d4caf8a5661
r3d5un <r3d5un@pm.me>
2024-08-26 20:18:04 +0200

    Implemented a basic task scheduler in the orchestrator

--- cmd/bookshelf/orchestrator/module.go
+++ cmd/bookshelf/orchestrator/module.go
@@ -3,18 +3,24 @@ package orchestrator
 import (
 	"context"
 	"log/slog"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/r3d5un/Bookshelf/internal/config"
+	"github.com/r3d5un/Bookshelf/internal/orchestrator"
+	"github.com/r3d5un/Bookshelf/internal/orchestrator/data"
+	"github.com/r3d5un/Bookshelf/internal/orchestrator/types"
 	"github.com/r3d5un/Bookshelf/internal/system"
 )
 
 const ModuleName string = "orchestrator"
 
 type Module struct {
-	logger *slog.Logger
-	cfg    *config.Config
-	db     *pgxpool.Pool
+	logger    *slog.Logger
+	cfg       *config.Config
+	db        *pgxpool.Pool
+	models    data.Models
+	scheduler *orchestrator.Scheduler
 }
 
 func (m *Module) Startup(ctx context.Context, mono system.Monolith) (err error) {
@@ -36,11 +42,26 @@ func (m *Module) Startup(ctx context.Context, mono system.Monolith) (err error) 
 	}
 	m.logger.Info("connection pool established")
 
+	timeout := time.Duration(m.cfg.DB.Timeout) * time.Second
+	m.models = data.NewModels(m.db, &timeout)
+
+	m.logger.Info("creating task scheduler")
+	m.scheduler = orchestrator.NewScheduler(&m.models)
+	taskName := "hello-world"
+	m.scheduler.AddCronJob(ctx, "* * * * *", types.Task{
+		Name: &taskName,
+	})
+	m.logger.Info("starting scheduler")
+	m.scheduler.Start()
+
 	return nil
 }
 
 func (m *Module) Shutdown() {
 	m.logger.Info("shutting down module")
+
+	m.logger.Info("stopping scheduler")
+	m.scheduler.Stop()
 
 	m.logger.Info("closing module connection pool")
 	m.db.Close()
