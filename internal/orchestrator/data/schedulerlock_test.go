be57b29c4198962f1a8a1649b0d938483e00fb12
r3d5un <r3d5un@pm.me>
2024-08-28 19:59:40 +0200

    Create test for the AcquireLock method

---
+++ internal/orchestrator/data/schedulerlock_test.go
@@ -0,0 +1,26 @@
+package data_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/uuid"
+)
+
+func TestSchedulerLock(t *testing.T) {
+	instanceID := uuid.New()
+
+	t.Run("AcquireLock", func(t *testing.T) {
+		lockAcquired, err := models.SchedulerLock.AcquireLock(context.Background(), instanceID)
+		if err != nil {
+			t.Errorf("an error occurred while acquiring the scheduler lock: %s\n", err)
+			return
+		}
+
+		if !lockAcquired {
+			t.Errorf("unable to acquire scheduler lock: %t", lockAcquired)
+			return
+		}
+	})
+
+}
