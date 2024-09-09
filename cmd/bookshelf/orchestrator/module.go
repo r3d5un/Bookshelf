24f09a1564b5bf4ddf054bfe2ad0556eabc969f8
r3d5un <r3d5un@pm.me>
2024-09-09 22:00:05 +0200

    Rename the task in the task collection

--- cmd/bookshelf/orchestrator/module.go
+++ cmd/bookshelf/orchestrator/module.go
@@ -65,7 +65,8 @@ func (m *Module) Startup(ctx context.Context, mono system.Monolith) (err error) 
 
 	m.logger.Info("creating task collection")
 	m.taskCollection = orchestrator.Collection{}
-	m.taskCollection.Add("hello-world", m.helloWorld)
+	// TODO: Make a more elegant way to initialize tasks
+	m.taskCollection.Add("Hello, World!", m.helloWorld)
 
 	m.logger.Info("creating task runner")
 	m.wg.Add(1)
