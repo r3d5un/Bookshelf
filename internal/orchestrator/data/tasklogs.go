a80b60873d1691ce6b43ee5a192d9a9c2dce0560
r3d5un <r3d5un@pm.me>
2024-09-13 21:49:15 +0200

    Create Insert method for the TaskLogModel

--- internal/orchestrator/data/tasklogs.go
+++ internal/orchestrator/data/tasklogs.go
@@ -2,7 +2,7 @@ package data
 
 import (
 	"context"
-	"database/sql"
+	"encoding/json"
 	"errors"
 	"log/slog"
 	"time"
@@ -15,14 +15,59 @@ import (
 )
 
 type TaskLog struct {
-	ID     uuid.UUID      `json:"id"`
-	TaskID uuid.UUID      `json:"taskId"`
-	Log    sql.NullString `json:"log"`
+	ID     uuid.UUID       `json:"id"`
+	TaskID uuid.UUID       `json:"taskId"`
+	Log    json.RawMessage `json:"log"`
 }
 
 type TaskLogModel struct {
 	Timeout *time.Duration
 	Pool    *pgxpool.Pool
+}
+
+func (m *TaskLogModel) Insert(ctx context.Context, newTaskLog TaskLog) (*TaskLog, error) {
+	query := `
+INSERT INTO orchestrator.task_logs (id,
+                                    task_id,
+                                    log)
+VALUES ($1::UUID,
+        $2::UUID,
+        $3::JSONB)
+RETURNING
+    id,
+    task_id,
+    log;
+`
+
+	ctx, cancel := context.WithTimeout(ctx, *m.Timeout)
+	defer cancel()
+
+	logger := logging.LoggerFromContext(ctx).With(slog.Group(
+		"query",
+		slog.String("query", database.MinifySQL(query)),
+		slog.Any("taskLog", newTaskLog),
+	))
+
+	taskLog := &TaskLog{}
+	logger.Info("performing query")
+	err := m.Pool.QueryRow(ctx, query, newTaskLog.ID, newTaskLog.TaskID, newTaskLog.Log).Scan(
+		&taskLog.ID,
+		&taskLog.TaskID,
+		&taskLog.Log,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, pgx.ErrNoRows):
+			logger.Info("no rows found")
+			return nil, ErrRecordNotFound
+		default:
+			logger.Error("an error occurred while performing query", "error", err)
+			return nil, err
+		}
+	}
+
+	logger.Info("returning task")
+	return taskLog, nil
 }
 
 func (m *TaskLogModel) Get(ctx context.Context, id uuid.UUID) (*TaskLog, error) {
--- internal/orchestrator/data/tasklogs_test.go
+++ internal/orchestrator/data/tasklogs_test.go
@@ -2,14 +2,64 @@ package data_test
 
 import (
 	"context"
+	"database/sql"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
+	"github.com/r3d5un/Bookshelf/internal/orchestrator/data"
 )
 
 func TestTaskLogModel(t *testing.T) {
+	task := data.Task{
+		Name:      "task_log_test",
+		CronExpr:  sql.NullString{String: "* * * * *", Valid: true},
+		Enabled:   sql.NullBool{Bool: false, Valid: true},
+		UpdatedAt: sql.NullTime{Time: time.Now(), Valid: true},
+	}
+	_, err := models.Tasks.Insert(context.Background(), task)
+	if err != nil {
+		t.Errorf("error occurred while inserting task: %s\n", err)
+		return
+	}
+
+	state := string(data.WaitingTaskState)
+	timestamp := time.Now()
+	taskQueueRow := data.TaskQueue{
+		Name:      &task.Name,
+		State:     &state,
+		CreatedAt: &timestamp,
+		UpdatedAt: &timestamp,
+		RunAt:     &timestamp,
+	}
+	insertedTaskQueue, err := models.TaskQueues.Insert(context.Background(), taskQueueRow)
+	if err != nil {
+		t.Errorf("error occurred while inserting new task: %s", err)
+		return
+	}
+
+	taskLog := data.TaskLog{
+		ID:     uuid.New(),
+		TaskID: insertedTaskQueue.ID,
+		Log: []byte(
+			`{"time":"2024-09-13T21:40:09.926641812+02:00","level":"INFO","msg":"test log statement"}`,
+		),
+	}
+
+	tql := data.TaskLog{}
+
+	t.Run("Insert", func(t *testing.T) {
+		insertedTaskLog, err := models.TaskLogs.Insert(context.Background(), taskLog)
+		if err != nil {
+			t.Errorf("error occurred while inserting task log: %s\n", err)
+			return
+		}
+
+		tql = *insertedTaskLog
+	})
+
 	t.Run("Get", func(t *testing.T) {
-		_, err := models.TaskLogs.Get(context.Background(), uuid.New())
+		_, err := models.TaskLogs.Get(context.Background(), tql.ID)
 		if err != nil {
 			t.Errorf("unable to get task log: %s\n", err)
 			return
