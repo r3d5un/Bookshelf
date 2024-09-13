fa99ac6a94c022cd914b34d66b09482b6bf3ec56
r3d5un <r3d5un@pm.me>
2024-09-14 01:01:10 +0200

    Implement database logger in 'Hello, World!" example task

--- cmd/bookshelf/orchestrator/helloworldtask.go
+++ cmd/bookshelf/orchestrator/helloworldtask.go
@@ -1,9 +1,42 @@
 package orchestrator
 
-import "context"
+import (
+	"context"
+	"errors"
+	"io"
+	"log/slog"
+	"os"
+
+	"github.com/google/uuid"
+	"github.com/r3d5un/Bookshelf/internal/orchestrator/types"
+)
+
+const (
+	TaskName string = "Hello, World!"
+)
 
 func (m *Module) helloWorld(ctx context.Context) error {
-	m.logger.InfoContext(ctx, "Hello, World!")
+
+	taskQueueID, ok := ctx.Value("taskQueueID").(uuid.UUID)
+	if !ok {
+		return errors.New("unable to get task queue ID from context")
+	}
+	logWriter := types.NewTaskLogWriter(ctx, &m.models, taskQueueID, 100)
+
+	go func() {
+		logWriter.LogSink(ctx)
+	}()
+
+	handler := slog.NewJSONHandler(io.MultiWriter(&logWriter, os.Stdout), nil)
+	logger := slog.New(handler).With(slog.Group(
+		"task",
+		slog.String("taskName", TaskName),
+		slog.String("taskId", taskQueueID.String())),
+	)
+
+	logger.InfoContext(ctx, "Hello, World!")
+
+	logWriter.Stop()
 
 	return nil
 }
