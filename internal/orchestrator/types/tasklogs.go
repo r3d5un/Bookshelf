ec9f6399514480fbaab78ca61532e325de04c4f4
r3d5un <r3d5un@pm.me>
2024-09-13 23:24:57 +0200

    Create TaskLogWriter to satisfy the io.Writer interface
    The intention is to have this be a non-blocking database log writer.

--- internal/orchestrator/types/tasklogs.go
+++ internal/orchestrator/types/tasklogs.go
@@ -3,6 +3,7 @@ package types
 import (
 	"context"
 	"encoding/json"
+	"log/slog"
 
 	"github.com/google/uuid"
 	"github.com/r3d5un/Bookshelf/internal/orchestrator/data"
@@ -12,6 +13,65 @@ type TaskLog struct {
 	ID     uuid.UUID       `json:"id"`
 	TaskID uuid.UUID       `json:"taskId"`
 	Log    json.RawMessage `json:"log"`
+}
+
+type TaskLogWriter struct {
+	taskID    uuid.UUID
+	Done      chan struct{}
+	logBuffer chan TaskLog
+	models    *data.Models
+}
+
+func NewTaskLogWriter(
+	ctx context.Context,
+	models *data.Models,
+	taskID uuid.UUID,
+	logBufferSize int,
+) TaskLogWriter {
+	return TaskLogWriter{
+		taskID:    taskID,
+		logBuffer: make(chan TaskLog, logBufferSize),
+		Done:      make(chan struct{}),
+		models:    models,
+	}
+}
+
+func (tlw *TaskLogWriter) Write(p []byte) (n int, err error) {
+	log := TaskLog{
+		ID:     uuid.New(),
+		TaskID: tlw.taskID,
+		Log:    p,
+	}
+
+	go func() {
+		tlw.logBuffer <- log
+	}()
+
+	return len(p), nil
+}
+
+func (tlw *TaskLogWriter) LogSink(ctx context.Context) {
+	for {
+		select {
+		case <-tlw.Done:
+			return
+		case log, ok := <-tlw.logBuffer:
+			if !ok {
+				slog.Error("unable to read logs from log buffer")
+				return
+			}
+			_, err := CreateTaskLog(ctx, tlw.models, log)
+			if err != nil {
+				slog.Error("unable to create log records", "error", err)
+				return
+			}
+		}
+	}
+}
+
+func (tlw *TaskLogWriter) Stop() {
+	close(tlw.logBuffer)
+	close(tlw.Done)
 }
 
 func CreateTaskLog(ctx context.Context, models *data.Models, log TaskLog) (*TaskLog, error) {
