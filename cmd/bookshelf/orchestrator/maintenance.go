ce0acd3f314544dd0ec7b3cb513950bb537d4f6c
r3d5un <r3d5un@pm.me>
2024-09-15 00:21:38 +0200

    Embed task logger in context

--- cmd/bookshelf/orchestrator/maintenance.go
+++ cmd/bookshelf/orchestrator/maintenance.go
@@ -2,7 +2,7 @@ package orchestrator
 
 import (
 	"context"
-	"log/slog"
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -14,34 +14,35 @@ import (
 const RemoveOldScheduledTask string = "Remove Old Scheduled Tasks"
 
 func (m *Module) removeOldScheduledTasks(ctx context.Context) error {
-	logger := logging.LoggerFromContext(ctx).With(
-		slog.Group(
-			"taskRun",
-			slog.String("id", uuid.New().String()),
-			slog.String("name", RemoveOldScheduledTask),
-		),
-	)
+	taskQueueID, ok := ctx.Value("taskQueueID").(uuid.UUID)
+	if !ok {
+		return errors.New("unable to get task queue ID from context")
+	}
 
-	logger.Info("removing old completed runs")
+	logger, stopLogger := types.NewTaskLogger(ctx, &m.models, RemoveOldScheduledTask, taskQueueID)
+	defer stopLogger()
+	ctx = context.WithValue(ctx, logging.LoggerKey, logger)
 
-	completedState := string(data.CompleteTaskState)
+	logger.Info("starting task schedule maintenance task")
+
 	monthAgo := time.Now().AddDate(0, -1, 0)
+	logger.Info("checking for old tasks", "date", monthAgo)
 	oldScheduledTasks, err := types.ReadAllScheudledTasks(ctx, &m.models, data.Filters{
-		Page:      1,
-		PageSize:  50_000,
-		State:     &completedState,
-		RunAtFrom: &monthAgo,
+		Page:     1,
+		PageSize: 50_000,
+		RunAtTo:  &monthAgo,
 	})
 	if err != nil {
 		logger.Info("unable to read tasks", "error", err)
 		return err
 	}
-
 	if oldScheduledTasks.TotalRecords < 1 {
 		logger.Info("no old tasks to delete")
 		return nil
 	}
+	logger.Info("old tasks retrieved", "totalTasks", oldScheduledTasks.TotalRecords)
 
+	logger.Info("removing old completed runs")
 	for _, scheduledTask := range oldScheduledTasks.Data {
 		logger.Info("deleting old task", "scheduledTask", scheduledTask)
 		_, err := types.DeleteScheduledTask(ctx, &m.models, scheduledTask.ID)
@@ -50,6 +51,8 @@ func (m *Module) removeOldScheduledTasks(ctx context.Context) error {
 			return err
 		}
 	}
+
+	logger.Info("task schedule maintenance complete")
 
 	return nil
 }
